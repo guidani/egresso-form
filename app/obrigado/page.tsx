5df1e153f2615e3f6d841fef91d59da918710df5
Guilherme Daniel <gui.dani13@gmail.com>
2023-05-31 10:39:08 -0300

    add radial progress

---
+++ app/obrigado/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Page() {
+  const [seconds, setSeconds] = useState(5);
+  const router = useRouter();
+  useEffect(() => {
+    if (seconds == 0) {
+      router.replace("/");
+    }
+    const timer = setTimeout(() => {
+      setSeconds((prevSeconds) => prevSeconds - 1);
+    }, 1000);
+    console.log(seconds);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [seconds, router]);
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-lg">
+          <h1 className="text-5xl font-bold mb-4">
+            Obrigado por particiar da pesquisa!
+          </h1>
+          {/* <Link href={"/"}>
+            <button className="btn btn-success">Voltar</button>
+          </Link> */}
+          <div
+            className="radial-progress bg-success"
+            style={{
+              // @ts-ignore
+              "--value": seconds * 20,
+              "--size": "6rem",
+              "--thickness": "2px",
+            }}
+          >
+            {seconds}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
